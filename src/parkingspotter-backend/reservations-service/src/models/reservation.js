bf0b06271d096fc9211979d034968ed89c0dbda3
Lorenzo Thomas Contessa <lorenzocontessa.dev@gmail.com>
2025-09-10 09:38:18 +0200

    ReservationsChecker introduced

--- src/parkingspotter-backend/reservations-service/src/models/reservation.js
+++ src/parkingspotter-backend/reservations-service/src/models/reservation.js
@@ -6,7 +6,7 @@ module.exports = (sequelize) => {
     carPlate: { type: DataTypes.STRING, allowNull: false},
     startTime: { type: DataTypes.DATE, allowNull: false },
     endTime: { type: DataTypes.DATE, allowNull: false },
-    status: { type: DataTypes.ENUM('active', 'cancelled', 'completed'), defaultValue: 'active' },
+    status: { type: DataTypes.ENUM('active', 'cancelled', 'completed', 'expired'), defaultValue: 'active' },
     parkingId: {
       type: DataTypes.INTEGER,
       allowNull: false
